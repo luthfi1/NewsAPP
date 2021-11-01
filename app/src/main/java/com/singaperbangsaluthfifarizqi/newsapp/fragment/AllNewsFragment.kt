65ba3a2ad7e39b0d80984c36d5617cdaeffa2fcc
Muhammad Luthfi Farizqi <luthfifarizqi23@gmail.com>
2021-11-01 08:18:02 +0700

    Setting TablayoutMediator

---
+++ app/src/main/java/com/singaperbangsaluthfifarizqi/newsapp/fragment/AllNewsFragment.kt
@@ -0,0 +1,20 @@
+package com.singaperbangsaluthfifarizqi.newsapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.singaperbangsaluthfifarizqi.newsapp.R
+
+class AllNewsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_news, container, false)
+    }
+
+    
+}
